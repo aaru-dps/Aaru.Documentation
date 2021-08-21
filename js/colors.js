7276403e15c4f3bc8ff6858c4c061f9e79f1ec6b
Natalia Portillo <claunia@claunia.com>
2021-08-21 17:02:34 +0100

    Adapt whole site to follow the documentation theme.

---
+++ js/colors.js
@@ -0,0 +1,17 @@
+const themeColors =
+    [
+        "#006412",
+        "#0000D3",
+        "#FF6403",
+        "#562C05",
+        "#DD0907",
+        "#F20884",
+        "#4700A5",
+        "#90713A",
+        "#1FB714",
+        "#02ABEA",
+        "#FBF305",
+    ];
+
+const chartDataLabelColor = "#ffffff";
+const chartTitleColor = "#e18fdc";
